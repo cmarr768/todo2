3ea13eb34af65796c529eda8c34fad997b18665d
Colin <cmarr768@gmail.com>
2019-01-31 22:17:17 -0500

    setting up initial identity server

--- idsrv/Startup.cs
+++ idsrv/Startup.cs
@@ -7,27 +7,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace idsrv
-{
-    public class Startup
-    {
+namespace idsrv {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        services.AddIdentityServer()
-            .AddDeveloperSigningCredential();
+        public void ConfigureServices (IServiceCollection services) {
+            // configure identity server with in-memory stores, keys, clients and resources
+            services.AddIdentityServer ()
+                .AddDeveloperSigningCredential ()
+                .AddInMemoryApiResources (Config.GetApiResources ())
+                .AddInMemoryClients (Config.GetClients ());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            }
 
-        app.UseIdentityServer();
+            app.UseIdentityServer ();
         }
     }
 }
