3c25a31f34b5441306db3ba6e35761e01bb7abb9
Colin <cmarr768@gmail.com>
2019-01-31 22:07:33 -0500

    adding identity server

--- idsrv/Startup.cs
+++ idsrv/Startup.cs
@@ -15,20 +15,19 @@ namespace idsrv
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+        services.AddIdentityServer()
+            .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+        app.UseIdentityServer();
         }
     }
 }
